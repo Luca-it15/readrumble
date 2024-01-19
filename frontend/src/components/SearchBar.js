b3e1e90bd7a3086ee5e1be27a9f4f74a194c8c61
Luca-it15 <chiocchettiluca15@gmail.com>
2024-01-19 09:47:02 +0100

    Explore styles

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,13 +1,39 @@
 import TextField  from "@mui/material/TextField";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
+import Button from "@mui/material-next/Button";
+import {SearchRounded } from "@mui/icons-material";
+import { Typography } from "@mui/material";
 
+const searchBar = {
+    backgroundColor: 'white', 
+    border: '3pt solid #1976d2', 
+    borderRadius: '15px', 
+    margin: '10px',
+    fontSize: '18pt'
+  }
+
+  const toggle = {
+    backgroundColor: '#1976d2', 
+    padding: '15px', 
+    margin: '5px',
+    border: '3pt solid #1976d2', 
+    borderRadius: '15px', 
+    color: 'white'
+  }
+
+  const icon = {
+    color: 'white'
+  }
 
 export default function SearchBar() {
         return (
             <Box component="form" sx={{ display: 'flex', alignItems: 'center' }}>
-                <TextField type="text" placeholder="Cerca" variant="outlined" sx={{ mr: 2 }} />
-                <Button variant="contained" color="primary">Cerca</Button>
+                <TextField type="text" placeholder="Cerca" variant="outlined" sx={searchBar} />
+                <Button 
+                            sx={toggle}
+                            variant="filledTonal" 
+                            startIcon={<SearchRounded sx={icon}/>}>
+                 <Typography variant='h5'>Cerca</Typography></Button>
             </Box>
         );
 }
--- frontend/src/pages/Explore.js
+++ frontend/src/pages/Explore.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {Paper, Grid} from '@mui/material';
+import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import ToggleButton from '@mui/material/ToggleButton'; 
 import ToggleButtonGroup from '@mui/material/ToggleButton'; 
@@ -8,21 +8,58 @@ import BookListShow from '../components/BookListShow';
 import SearchBar from '../components/SearchBar';
 import UserList from '../components/UserList';
 import PostsListAll from '../components/PostsListAll';
+import { Paper } from '@mui/material';
 
 export default function Explore() {
  const [value, setValue] = useState(0); 
+ const [toggle1, setToggle1] = useState(true); 
+ const [toggle2, setToggle2] = useState(false); 
+ const [toggle3, setToggle3] = useState(false); 
   
+  
+  
+ const PaperStyle = {
+  backgroundColor: '#f1f7fa',
+  padding: '10px',
+  margin: '10px',
+  borderRadius: 5
+}
+ 
+ const toggle = {
+   backgroundColor: '#1976d2', 
+   padding: '15px', 
+   margin: '5px',
+   border: '3pt solid #1976d2', 
+   borderRadius: '15px', 
+   color: 'white'
+ }
+
+ const toggleGroup = {
+  backgroundColor: 'white', 
+  border: '3pt solid #1976d2', 
+  borderRadius: '15px', 
+}
+
 
   function getBook() {
     setValue(0); 
+    setToggle1(true);
+    setToggle2(false);
+    setToggle3(false);
   }
 
   function getPost() {
     setValue(1); 
+    setToggle1(false);
+    setToggle2(true);
+    setToggle3(false);
   }
 
   function getUser() {
     setValue(2); 
+    setToggle1(false);
+    setToggle2(false);
+    setToggle3(true);
   }
 
   const Choiche = ({value}) => {
@@ -58,41 +95,33 @@ export default function Explore() {
         ); 
      }
    }
-   
-   const PaperStyle = {
-    backgroundColor: '#f1f7fa',
-    padding: '10px',
-    margin: '10px',
-    borderRadius: 5
-}
-
 
         return (
           <Paper sx={PaperStyle}>
-            <Grid container direction="row" alignItems="center" justifyContent="center" item xs={11} md={11}>
-            <Typography variant="h2">Esplora</Typography>
-             <Grid item sx={1} md={12} alignItems="center" justifyContent="center" container direction="row">
-                <SearchBar />
+            <Container>
+                <Typography variant="h2">Esplora</Typography>
+                <SearchBar/>
+                <Container className='explore'>
                 <ToggleButtonGroup
                     exclusive
                     aria-label="explore"
-                    color={"primary"}
                     size="large"
                     role="group"
+                    sx={toggleGroup}
                 >
-                    <ToggleButton onClick={getBook} className='toggle' value="book" aria-label="book">
+                    <ToggleButton onClick={getBook} sx={toggle} value="Book" aria-label="book" disabled={toggle1}>
                        <h4>Book</h4>
                     </ToggleButton>
-                    <ToggleButton onClick={getPost} className='toggle' value="review" aria-label="review">
+                    <ToggleButton onClick={getPost} sx={toggle} value="Post" aria-label="post" disabled={toggle2}>
                        <h4>Post</h4>
                     </ToggleButton>
-                    <ToggleButton onClick={getUser} className='toggle' value="user" aria-label="user">
+                    <ToggleButton onClick={getUser} sx={toggle} value="User" aria-label="user" disabled={toggle3}>
                        <h4>User</h4>
                     </ToggleButton>
                 </ToggleButtonGroup>
-                </Grid>
                   <Choiche  value={value} />
-                </Grid> 
+                </Container>
+            </Container>
             </Paper>
         );
 
