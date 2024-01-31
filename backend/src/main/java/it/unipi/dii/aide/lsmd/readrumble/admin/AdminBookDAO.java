5b1bd634488857606646f3429a019e3a9118a1a3
Luca-it15 <chiocchettiluca15@gmail.com>
2024-01-31 23:21:49 +0100

    handler book by admin

--- backend/src/main/java/it/unipi/dii/aide/lsmd/readrumble/admin/AdminBookDAO.java
+++ backend/src/main/java/it/unipi/dii/aide/lsmd/readrumble/admin/AdminBookDAO.java
@@ -4,41 +4,87 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.UpdateResult;
+import it.unipi.dii.aide.lsmd.readrumble.config.database.Neo4jConfig;
 import org.bson.Document;
 
 import it.unipi.dii.aide.lsmd.readrumble.config.database.MongoConfig;
 import it.unipi.dii.aide.lsmd.readrumble.book.Book;
 import org.bson.conversions.Bson;
+import org.neo4j.driver.Session;
 import org.springframework.http.ResponseEntity;
 
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Updates.set;
 
 public class AdminBookDAO {
-    public ResponseEntity<String> removeBook(long id) {
-        MongoCollection<Document> collection = MongoConfig.getCollection("Books");
-        Document query = new Document("_id", id);
-        Document target = collection.find(query).first();
-        if(target != null) {
-            collection.deleteOne(target);
-            return ResponseEntity.ok("book remove successfull");
-        } else
-            return ResponseEntity.ok("failed to remove the book");
+
+    private static List<Book> bookToAdd;
+    private static List<Document> bookToUpdate;
+    private static List<Long> bookToRemove;
+    private final MongoCollection<Document> collection = MongoConfig.getCollection("Books");
+
+    public ResponseEntity<String> addBookTemp(Book book) {
+        if(bookToAdd == null)
+            bookToAdd = new ArrayList<>();
+        bookToAdd.add(book);
+        return ResponseEntity.ok("book will add");
     }
 
-    public ResponseEntity<String> addBook(Book book) {
+    public ResponseEntity<String> updateBookTemp(Document update) {
+        if(bookToUpdate == null)
+            bookToUpdate = new ArrayList<>();
+        bookToUpdate.add(update);
+        return ResponseEntity.ok("book will update");
+    }
 
-        MongoCollection<Document> collection = MongoConfig.getCollection("Books");
+    public ResponseEntity<String> removeBookTemp(long id) {
+        if(bookToRemove == null)
+            bookToRemove = new ArrayList<>();
+        return ResponseEntity.ok("book will remove");
+    }
 
-        Instant instant = Instant.now();
-        long timestamp = instant.toEpochMilli();
-          Document document = new Document("_id", timestamp)
+    public void removeBook() {
+
+        if(bookToRemove.isEmpty())
+            return;
+
+        Bson filter = Filters.in("_id", bookToRemove);
+
+        DeleteResult result = collection.deleteMany(filter);
+
+        try (Session session = Neo4jConfig.getSession()) {
+            List<String> idStrings = bookToRemove.stream().map(Object::toString).collect(Collectors.toList());
+            String query = String.format("MATCH (l:Libro) WHERE l.id IN %s DETACH DELETE l", idStrings);
+            session.run(query);
+        }
+
+        bookToRemove.clear();
+
+        if (result.wasAcknowledged())
+            System.out.println("book remove successfull: book removed" + result.getDeletedCount());
+        else
+           System.err.println("failed to remove the book: book removed" + result.getDeletedCount());
+    }
+
+    public void addBook() {
+
+        if(bookToAdd.isEmpty())
+            return;
+
+        List<Document> bookAdd = new ArrayList<>();
+        for (Book book : bookToAdd) {
+
+            Instant instant = Instant.now();
+            long timestamp = instant.toEpochMilli();
+            Document document = new Document("_id", timestamp)
                     .append("isbn", book.getIsbn())
                     .append("description", book.getDescription())
                     .append("link", book.getLink())
@@ -50,58 +96,81 @@ public class AdminBookDAO {
                     .append("image_url", book.getImage_url())
                     .append("title", book.getTitle())
                     .append("tags", book.getTags());
-            collection.insertOne(document);
 
-        return ResponseEntity.ok("book add successfull");
+            bookAdd.add(document);
         }
+        collection.insertMany(bookAdd);
 
-    public ResponseEntity<String> updateBook(Document changes)
-    {
-        int new_field_int = 0;
-        String new_field_String = null;
-        List<String> new_field_list = new ArrayList<>();
-        int index = 0;
-        String type_of_change_request = (String) changes.get("type_of_change_request");
-        if(type_of_change_request.equals("num_pages") || type_of_change_request.equals("publication_year")) {
-            new_field_int = changes.getInteger("new_field");
-            index = 0;
-        } else if(type_of_change_request.equals("tags") || type_of_change_request.equals("authors")) {
-          new_field_list =  (List<String>) changes.get("new_field");
-          index = 1;
+        try (Session session = Neo4jConfig.getSession()) {
 
-        } else {
-            new_field_String = changes.getString("new_field");
-            index = 2;
-        }
-        long id_to_use = Long.parseLong(changes.getString("id_to_use"));
-        MongoCollection<Document> collection = MongoConfig.getCollection("Books");
-        Document book = collection.find(eq("_id", id_to_use)).first();
-        String result = null;
-         try {
-             if(book != null) {
-                 switch (index) {
-                     case 0:
-                         collection.updateOne(eq("_id", id_to_use), set(type_of_change_request, new_field_int));
-                         result = (String) type_of_change_request + " Changed in " +  new_field_int;
-                         break;
-                     case 1:
-                         collection.updateOne(eq("_id", id_to_use), set(type_of_change_request, new_field_list));
-                          result = (String) type_of_change_request + " Changed in " +  new_field_list;
-                         break;
-                     default:
-                         collection.updateOne(eq("_id", id_to_use), set(type_of_change_request, new_field_String));
-                         result = (String) type_of_change_request + " Changed in " +  new_field_String;
-                 }
-                 return ResponseEntity.ok(result);
-             }
-            else
-            {
-                return ResponseEntity.ok("NOT FOUND");
+            for (int i = 0; i < bookToAdd.size(); i++) {
+                String query = String.format("CREATE (l:Libro {id: '%s', title: '%s'})", bookAdd.get(i).getLong("_id"), bookAdd.get(i).getString("title"));
+                session.run(query);
             }
         }
-        catch(Exception e)
-        {
-            return ResponseEntity.ok("EXCEPTION IN SERVER");
+        bookToAdd.clear();
+       System.out.println("book add successfull");
+    }
+
+    public void updateBook() {
+
+        if(bookToUpdate.isEmpty())
+            return;
+
+        String result = null;
+        for (Document changes : bookToUpdate) {
+
+            int new_field_int = 0;
+            String new_field_String = null;
+            List<String> new_field_list = new ArrayList<>();
+            int index = 0;
+            String type_of_change_request = changes.getString("type_of_change_request");
+            if (type_of_change_request.equals("num_pages") || type_of_change_request.equals("publication_year")) {
+                new_field_int = changes.getInteger("new_field");
+            } else if (type_of_change_request.equals("tags") || type_of_change_request.equals("authors")) {
+                new_field_list = (List<String>) changes.get("new_field");
+                index = 1;
+
+            } else {
+                new_field_String = changes.getString("new_field");
+                index = 2;
+            }
+            long id_to_use = Long.parseLong(changes.getString("id_to_use"));
+            MongoCollection<Document> collection = MongoConfig.getCollection("Books");
+            Document book = collection.find(eq("_id", id_to_use)).first();
+
+            try {
+                if (book != null) {
+                    switch (index) {
+                        case 0:
+                            collection.updateOne(eq("_id", id_to_use), set(type_of_change_request, new_field_int));
+                            result = type_of_change_request + " Changed in " + new_field_int;
+                            break;
+                        case 1:
+                            collection.updateOne(eq("_id", id_to_use), set(type_of_change_request, new_field_list));
+                            result = (String) type_of_change_request + " Changed in " + new_field_list;
+                            break;
+                        default:
+                            collection.updateOne(eq("_id", id_to_use), set(type_of_change_request, new_field_String));
+                            result = (String) type_of_change_request + " Changed in " + new_field_String;
+                            if(type_of_change_request.equals("title")) {
+                                try (Session session = Neo4jConfig.getSession()) {
+                                    String query = String.format("CREATE (l:Libro {id: '%s', titolo: '%s'})",id_to_use,new_field_String );
+                                    session.run(query);
+                                }
+                            }
+                    }
+
+                } else {
+                    result = "NOT FOUND";
+                }
+            } catch (Exception e) {
+                System.err.println("EXCEPTION IN SERVER");
+                return;
+            }
+
         }
+        bookToUpdate.clear();
+        System.out.println(result);
     }
 }
--- backend/src/main/java/it/unipi/dii/aide/lsmd/readrumble/data_migration/MemoryToMongo.java
+++ backend/src/main/java/it/unipi/dii/aide/lsmd/readrumble/data_migration/MemoryToMongo.java
@@ -1,18 +1,37 @@
 package it.unipi.dii.aide.lsmd.readrumble.data_migration;
 
+import it.unipi.dii.aide.lsmd.readrumble.admin.AdminBookDAO;
 import it.unipi.dii.aide.lsmd.readrumble.admin.AdminCompetitionDAO;
 import it.unipi.dii.aide.lsmd.readrumble.config.database.MongoConfig;
 import it.unipi.dii.aide.lsmd.readrumble.user.UserDAO;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 @Component
 public class MemoryToMongo {
     private UserDAO userDAO;
     private AdminCompetitionDAO adminCompetitionDAO;
+    private AdminBookDAO adminBookDAO;
+
+
     public MemoryToMongo() {
         this.userDAO = new UserDAO();
         this.adminCompetitionDAO = new AdminCompetitionDAO();
+        this.adminBookDAO = new AdminBookDAO();
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+        long initialDelay = LocalTime.now().until(LocalTime.MIDNIGHT, ChronoUnit.MINUTES);
+        long period = 24 * 60;  // Ripeti ogni 24 ore
+
+        scheduler.scheduleAtFixedRate(new HandlerBookAdmin(), initialDelay, period, TimeUnit.MINUTES);
+
     }
 
     /**
@@ -26,4 +45,15 @@ public class MemoryToMongo {
     public void insertNewCompetitionsIntoMongoDB() {
         adminCompetitionDAO.saveInMemoryCompetitions();
     }
+
+
+    public class HandlerBookAdmin implements Runnable {
+        @Override
+        public void run() {
+            adminBookDAO.addBook();
+            adminBookDAO.updateBook();
+            adminBookDAO.removeBook();
+        }
+    }
+
 }
