7f360f929d8d8b078fa0d44c0fbee3d7072ababb
francesco-crispino <f.crispino1@studenti.unipi.it>
2024-02-07 18:02:47 +0100

    Daemon cleaning and semaphores removed in memToMongo and memToNeo

--- backend/src/main/java/it/unipi/dii/aide/lsmd/readrumble/data_migration/MemoryToMongo.java
+++ backend/src/main/java/it/unipi/dii/aide/lsmd/readrumble/data_migration/MemoryToMongo.java
@@ -2,14 +2,14 @@ package it.unipi.dii.aide.lsmd.readrumble.data_migration;
 
 import it.unipi.dii.aide.lsmd.readrumble.admin.AdminBookDAO;
 import it.unipi.dii.aide.lsmd.readrumble.admin.AdminCompetitionDAO;
-import it.unipi.dii.aide.lsmd.readrumble.config.database.MongoConfig;
 import it.unipi.dii.aide.lsmd.readrumble.user.UserDAO;
-import it.unipi.dii.aide.lsmd.readrumble.utils.SemaphoreRR;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
+
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -20,7 +20,6 @@ public class MemoryToMongo {
     private AdminCompetitionDAO adminCompetitionDAO;
     private AdminBookDAO adminBookDAO;
 
-
     public MemoryToMongo() {
         this.userDAO = new UserDAO();
         this.adminCompetitionDAO = new AdminCompetitionDAO();
@@ -29,54 +28,30 @@ public class MemoryToMongo {
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
         long initialDelay = LocalTime.now().until(LocalTime.MIDNIGHT, ChronoUnit.MINUTES);
-        long period = 24 * 60;  // Ripeti ogni 24 ore
+        long period = 24 * 60;
 
         scheduler.scheduleAtFixedRate(new HandlerBookAdmin(), initialDelay, period, TimeUnit.MINUTES);
-
     }
 
     /**
-     * This method saves the users in memory to the document DB every hour
+     * This method saves the users in memory to the document DB and graph DB every hour
      */
-    @Scheduled(fixedRate = 3600000, initialDelay =3600000 ) // 1 hour
+    @Scheduled(fixedRate = 3600000, initialDelay = 3600000) // 1 hour
     public void saveInMemoryUsers() {
-
-        SemaphoreRR semaphore = SemaphoreRR.getInstance(1);
-        try {
-            semaphore.acquire();
-        }catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         userDAO.saveInMemoryUsers();
-        semaphore.release();
-    }
-    @Scheduled(fixedRate =  86400000, initialDelay =3600000) // 24 hours in milliseconds
-    public void insertNewCompetitionsIntoMongoDB() {
-        SemaphoreRR semaphore = SemaphoreRR.getInstance(1);
-        try {
-            semaphore.acquire();
-        }catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        adminCompetitionDAO.saveInMemoryCompetitions();
-        semaphore.release();
     }
 
+    @Scheduled(fixedRate = 86400000, initialDelay = 3600000) // 24 hours in milliseconds
+    public void insertNewCompetitionsIntoMongoDB() {
+        adminCompetitionDAO.saveInMemoryCompetitions();
+    }
 
     public class HandlerBookAdmin implements Runnable {
         @Override
         public void run() {
-            SemaphoreRR semaphore = SemaphoreRR.getInstance(1);
-            try {
-                semaphore.acquire();
-            }catch (InterruptedException e) {
-                e.printStackTrace();
-            }
             adminBookDAO.addBook();
             adminBookDAO.updateBook();
             adminBookDAO.removeBook();
-            semaphore.release();
         }
     }
-
 }
--- backend/src/main/java/it/unipi/dii/aide/lsmd/readrumble/data_migration/MemoryToNeo4j.java
+++ backend/src/main/java/it/unipi/dii/aide/lsmd/readrumble/data_migration/MemoryToNeo4j.java
@@ -2,7 +2,7 @@ package it.unipi.dii.aide.lsmd.readrumble.data_migration;
 
 import it.unipi.dii.aide.lsmd.readrumble.book.BookDAO;
 import it.unipi.dii.aide.lsmd.readrumble.user.UserDAO;
-import it.unipi.dii.aide.lsmd.readrumble.utils.SemaphoreRR;
+
 import org.springframework.scheduling.annotation.Scheduled;
 
 public class MemoryToNeo4j {
@@ -14,64 +14,18 @@ public class MemoryToNeo4j {
         this.bookDAO = new BookDAO();
     }
 
-    /**
-     * This method saves the new follow relations in memory to the graph DB every hour
-     */
     @Scheduled(fixedRate = 3600000) // 1 hour
-    public void saveInMemoryFollowRelations() {
-        SemaphoreRR semaphore = SemaphoreRR.getInstance(1);
-        try {
-            semaphore.acquire();
-        }catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public void saveInMemoryData() {
+        // This method saves the new follow relations in memory to the graph DB
         userDAO.saveInMemoryFollowRelations();
-        semaphore.release();
-    }
 
-    /**
-     * This method saves the follow relations to be deleted in memory to the document DB every hour
-     */
-    @Scheduled(fixedRate = 3600000) // 1 hour
-    public void saveInMemoryFollowRelationsToBeDeleted() {
-
-        SemaphoreRR semaphore = SemaphoreRR.getInstance(1);
-        try {
-            semaphore.acquire();
-        }catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        // This method saves the follow relations to be deleted in memory to the document DB
         userDAO.saveInMemoryFollowRelationsToBeDeleted();
-        semaphore.release();
-    }
 
-    /**
-     * This method saves the favorite books in memory to the graph DB every hour
-     */
-    @Scheduled(fixedRate = 3600000) // 1 hour
-    public void saveInMemoryFavoriteBooks() {
-        SemaphoreRR semaphore = SemaphoreRR.getInstance(1);
-        try {
-            semaphore.acquire();
-        }catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        // This method saves the favorite books in memory to the graph DB
         bookDAO.saveInMemoryFavoriteBooks();
-        semaphore.release();
-    }
 
-    /**
-     * This method saves the favorite books to be deleted in memory to the document DB every hour
-     */
-    @Scheduled(fixedRate = 3600000) // 1 hour
-    public void saveInMemoryFavoriteBooksToBeDeleted() {
-        SemaphoreRR semaphore = SemaphoreRR.getInstance(1);
-        try {
-            semaphore.acquire();
-        }catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        // This method saves the favorite books to be deleted in memory to the document DB
         bookDAO.saveInMemoryFavoriteBooksToBeDeleted();
-        semaphore.release();
     }
 }
